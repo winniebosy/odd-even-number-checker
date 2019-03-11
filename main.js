4400104937115fa49c743cd5fbca92dadc9534ce
Winnie <winniebosy@gmail.com>
2019-03-12 02:40:35 +0300

    major changes in the css and html files. The js file still not rendering results

--- main.js
+++ main.js
@@ -1,6 +1,37 @@
-"use strict"
+function checkNumber(event){
+
+    let num = document.getElementById("").addEventListener()
 
 
-document.getElementById("oddevennum").addEventListener('input', function(){
-     console.log(12345);
+
+}
+
+
+
+
+
+
+    
+
+
+
+/*
+
+let num = e.target.value;
+let templatee = `is even`;
+let templateo = `is odd`;
+    if (num %2==0){
+        document.getElementById("output").innerHTML = num  +  templatee ;
+
+    }
+    else{
+        document.getElementById("output").innerHTML = num  +  templateo ;
+    }
+document.getElementById("button").addEventListener("click", function(e){ 
+      
 });
+
+
+
+
+*/ 
