2aaabc6504c61457b119cd53500d77aa54555f0a
Winnie <winniebosy@gmail.com>
2019-03-12 16:27:19 +0300

    successfully created an event for the js project

--- main.js
+++ main.js
@@ -1,16 +1,28 @@
 
 
-window.onload = function(){
-let input = document.querySelector('#oddeven');
-input.addEventListener('input', function(e){
-   let num =e.target.value;
-   
-    let btn = document.querySelector('#btn').addEventListener('click' ,function(){
-    console.log(e);
-    console.log("heyyyyy");
+window.onload = function() { // this facilitates in loading this js file once the page loads
+
+let input = document.querySelector('#oddeven',);//declares the variable input
+input.addEventListener('input', function(e){ // creates an input event when a user enters a number
+   let num =e.target.value; // declares variable for user input
+
+    let btn = document.querySelector('#btn', 50000).addEventListener('click' , function(){ //creates an event to facilitate an action once a user clicks the check number button
+     
+        const output = e.target.value; //creates const output.
+        
+      //condition to check whether the number is even or not
+        if ( num %2==0){
+            document.getElementById("output").innerHTML = num + " " + "is even";
+
+        }else{
+            document.getElementById("output").innerHTML = num + " " + "is odd";//
+            
+        }
+    
+});
+
 });
  
-});
 
  
 }
